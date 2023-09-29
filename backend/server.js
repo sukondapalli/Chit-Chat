32a05b87ed52964317caeda83c3d482ae936d1d7
sukondapalli <kondapalli_s@ece.iitr.ac.in>
2023-09-29 15:25:52 +0530

    Added dummy data and nodemon library

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,26 @@
 const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
 
 const app = express();
+dotenv.config();
 
-// creating an express api so that we can get
-
-app.get('/', (req,res) => {
-    res.send("API is running")
+// creating an express api so that we can get values from database
+app.get("/", (req, res) => {
+  res.send("API is running");
 });
 
-app.listen(5500, console.log("Server started on port 5500"));
+// get all the chats
+app.get("/api/chat", (req, res) => {
+  res.send(chats);
+});
 
+// api to get information of one particular chat using chat ID
+app.get("/api/chat/:id", (req, res) => {
+  current_id = req.params.id;
+  const singleChat = chats.find((chat) => chat._id == current_id);
+  res.send(singleChat);
+});
+
+const PORT = process.env.PORT || 5500;
+app.listen(5500, console.log("Server started on port ${PORT}"));
