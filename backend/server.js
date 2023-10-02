8fa4a25da196e2de38cae139787df2f46a7770fd
sukondapalli <kondapalli_s@ece.iitr.ac.in>
2023-10-02 21:28:21 +0530

    Initialised the front-end

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,6 @@ const { chats } = require("./data/data");
 const app = express();
 dotenv.config();
 
-// creating an express api so that we can get values from database
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
-
 // get all the chats
 app.get("/api/chat", (req, res) => {
   res.send(chats);
@@ -22,5 +17,5 @@ app.get("/api/chat/:id", (req, res) => {
   res.send(singleChat);
 });
 
-const PORT = process.env.PORT || 5500;
-app.listen(5500, console.log("Server started on port ${PORT}"));
+const PORT = process.env.PORT || 5000;
+app.listen({ PORT }, console.log("Server started on port ${PORT}"));
